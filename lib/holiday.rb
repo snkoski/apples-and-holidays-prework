fd584fc4676b6a83ac2098028f555709595f1dcc
Shawn Koski <shawnnkoski@gmail.com>
2018-04-05 01:29:12 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -97,7 +97,7 @@ def all_supplies_in_holidays(holiday_hash)
       print day_joined + ": "
       
       supplies.each do |supply|
-        print supplies.index(supply) == supplies.size ? "#{supply}" : "#{supply}, "
+        supplies.index(supply) == supplies.size ? "puts #{supply}" : "#{supply}, "
         #print supply
       end
     end
