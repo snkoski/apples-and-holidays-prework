6ccaa0c17cb0b0ffcbf21c5962b96aadcd3f7a75
Shawn Koski <shawnnkoski@gmail.com>
2018-04-05 01:12:23 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,6 +88,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts season.to_s.capitalize + ":"
     holiday.each do |holiday, supplies|
+      day = []
       day = holiday.to_s.split
       puts day
       #holiday.split
